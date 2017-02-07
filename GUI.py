57721cec1eaf0c5ea0bc736c6a728f53dd4f45d0
Alex Falkengren <alex@debian-x230.indigo>
2017-02-07 19:35:01 +0000

    Added GUI

---
+++ GUI.py
@@ -0,0 +1,11 @@
+import paho.mqtt.client as mqtt
+from Tkinter import *
+
+# MQTT 
+# Create GUI
+root = Tk()
+w = Label(root, text="Hello, world!")
+w.pack()
+root.mainloop()
+
+# Plot GUI https://plot.ly/python/time-series/
