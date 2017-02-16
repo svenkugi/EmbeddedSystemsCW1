c8e6eff0a2bea377b2be5634458f199b7c6fc47c
Polar <afa>
2017-02-16 08:44:38 +0000

    complete python script

--- sendREST.py
+++ sendREST.py
@@ -2,34 +2,39 @@ import requests
 import json
 import paho.mqtt.client as mqtt
 from datetime import datetime
+import json, datetime
+import time
 
 #CREATE TABLE sensorData (id INT NOT NULL IDENTITY(1,1), machine_id INT, time_data datetime, temperature float, brightness float, presence INT, humidity float);
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("esys/RushB/Data")
+	print("Connected to broker with result code "+str(rc))
+	client.subscribe("$SYS/broker/clients/connected")
+	client.subscribe("esys/RushB/+")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-	print(msg.topic+" "+str(msg.payload))
-	jsonData = json.loads(msg.payload.decode('utf-8'))
-	jsonData["Time"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	jsonData["MachineId"] = int(jsonData["MachineId"],16)
-	#data = {"id": 100, "time": "01/11/95 12:00:32.001", "data": {"presence": 1, "lux": 300, "temperature": 22}}
-	#data_json = json.dumps(data)
-	print(jsonData)
-	response = requests.post("http://embeddedsystems.azurewebsites.net/api.php", data=json.dumps(jsonData))
-	print(response)
-	print(response.text)
-
+	if msg.topic == "esys/RushB/Init":
+		print("KnockKnock connected, sending time:")
+		time.sleep(2)
+		j = {"date":datetime.datetime.now().isoformat()}
+		client.publish("esys/RushB/time", json.dumps(j))
+	elif msg.topic == "esys/RushB/Data":
+		print(msg.topic+" "+str(msg.payload))
+		jsonData = json.loads(msg.payload.decode('utf-8')) #decode from bytes to utf-8 and convert to JSON
+		jsonData["MachineId"] = int(jsonData["MachineId"],16) #convert hex machineid to hex
+		response = requests.post("http://embeddedsystems.azurewebsites.net/api.php", data=json.dumps(jsonData))
+		print(response)
+	else:
+		print(msg.topic+" "+str(msg.payload))
+		
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("localhost", 1883, 60)
+j = {"date":datetime.datetime.now().isoformat()}
+client.publish("esys/RushB/time", json.dumps(j))
 client.loop_forever()
 
