e08bf00c1ea1d76729ed7295b2f6845e9974fb60
indigo- <quixoticopposites@gmail.com>
2017-02-18 21:42:05 +0000

    Moved code

--- mqttHostScripts/RUN.bat
+++ mqttHostScripts/RUN.bat
@@ -1,2 +1,2 @@
 start "mosquitto" "D:\Program Files (x86)\mosquitto\mosquitto.exe" -v
-python sendREST.py
+python mqttHost.py
--- mqttHostScripts/mqttHost.py
+++ mqttHostScripts/mqttHost.py
@@ -10,31 +10,35 @@ import time
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
 	print("Connected to broker with result code "+str(rc))
-	client.subscribe("$SYS/broker/clients/connected")
 	client.subscribe("esys/RushB/+")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-	if msg.topic == "esys/RushB/Init":
+	if msg.topic == "esys/RushB/Init":	# If we get an initialization message from our device, send the time
 		print("KnockKnock connected, sending time:")
-		time.sleep(2)
+		time.sleep(2)	# Account for slight delay after device publishes message to when it can accept messages
 		j = {"date":datetime.datetime.now().isoformat()}
 		client.publish("esys/RushB/time", json.dumps(j))
-	elif msg.topic == "esys/RushB/Data":
+	elif msg.topic == "esys/RushB/Data": # If it is a data message, then upload via POST to cloud API
 		print(msg.topic+" "+str(msg.payload))
-		jsonData = json.loads(msg.payload.decode('utf-8')) #decode from bytes to utf-8 and convert to JSON
-		jsonData["MachineId"] = int(jsonData["MachineId"],16) #convert hex machineid to hex
+		jsonData = json.loads(msg.payload.decode('utf-8')) # decode from bytes to utf-8 and convert to JSON
+		jsonData["MachineId"] = int(jsonData["MachineId"],16) # convert hex machineid to hex
 		response = requests.post("http://embeddedsystems.azurewebsites.net/api.php", data=json.dumps(jsonData))
 		print(response)
 	else:
 		print(msg.topic+" "+str(msg.payload))
 		
 client = mqtt.Client()
+
+# Create callbacks
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("localhost", 1883, 60)
+
+# Send an extra time message on startup in case this script needs to restart
 j = {"date":datetime.datetime.now().isoformat()}
 client.publish("esys/RushB/time", json.dumps(j))
+
 client.loop_forever()
 
